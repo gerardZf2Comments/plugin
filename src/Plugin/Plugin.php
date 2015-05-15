f3036fb19c18d4e3bbaf146af372a4bf414d1086
Your Name <you@example.com>
2015-05-15 11:18:48 +0200

    first commit

---
+++ src/Plugin/Plugin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Plugin;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    protected $composer;
+    protected $io;
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            PluginEvents::POST_PACKAGE_INSTALL => array(
+                array('plugin', 0)
+            ),
+        );
+    }
+
+    public function plugin($event)
+    {
+        die(var_dump($event));
+    }
+}
